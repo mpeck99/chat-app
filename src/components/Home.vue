dbc4117bbe93ee5f420650b31f54a8440a0e4d74
mlpeck <mlpeck@arkbluecross.com>
2021-04-26 13:58:38 -0500

    rearranging the inputs to be inline and adding something to see if there are any messages to hide the errors list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,21 +2,24 @@
   <div class="wrapper">
       <h1>Chat</h1>
       <p>Welcome to my chat app. Please enter your name and select which user type you are and hit connect to enter a chat.</p>
-      <ul class="errors">
-          <li v-for="(error, index) in errorMsgs" :key="index">{{error}}</li>
-      </ul>
-      <form action="">
-          <div class="form-group">
-              <label for="name">Name:</label>
-              <input type="text" name="name" id="name">
-          </div>
-          <fieldset class="fieldset">
-              <legend>User type</legend>
-              <label for="agent"><input type="radio" name="type" id="agent" value="agent">Agent</label>
-              <label for="customer"><input type="radio" name="type" id="customer" value="customer">Customer</label>             
-          </fieldset>
-          <input type="button" value="Connect" @click="storeData" class="btn">
-      </form>
+      <form action="" class="form-group-inline">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="form-group">
+           <label for="type">User type</label>
+            <select name="type" id="type">
+                <option value="Select user type">Select user type</option>
+                <option value="agent">Agent</option>
+                <option value="customer">Customer</option>
+            </select> 
+        </div>
+        <input type="button" value="Connect" @click="storeData" class="btn">
+        <ul v-if="errorMsgs[0]" class="errors">
+            <li v-for="(error, index) in errorMsgs" :key="index">{{error}}</li>
+        </ul>
+    </form>
   </div>
 </template>
 
@@ -33,7 +36,7 @@ export default {
     methods :{
         storeData(){
             const name = document.querySelector('#name').value;
-            const userType = document.querySelector('input[name="type"]:checked')
+            const userType = document.querySelector('#type').value;
             this.errorMsgs =[];
             if(name === null || name === undefined || name ==="" || name ===" "){
                 this.errorMsgs.push('Please enter a name');
@@ -43,8 +46,8 @@ export default {
                 this.name = name;
             }
             
-            if(userType){
-                this.userType = userType.value;
+            if(userType != 'Select user type'){
+                this.userType = userType;
             }
             else {
                 this.errorMsgs.push('Please select a user type.')
@@ -63,28 +66,104 @@ export default {
 }
 </script>
 
-<style>
-input[type="button"] {
-    margin: 1rem;
-}
-
+<style lang="scss">
 .errors {
-    padding-left: 0;
+    width: calc(100% - 2rem);
+
+    position: absolute;
+    bottom: -8rem;
+
+    padding: 1rem;
 
     list-style-type: none;
 
     color: var(--red);
-}
 
-.errors li {
+    background-color: lighten(#c14b46, 35%);
+    border-radius: 0.25rem;
+
+    li {
     margin-bottom: 0.5rem;
+    }
 }
 
-.fieldset {
-    padding-left: 0;
-    margin-left: 0;
+.form-group {
+    position: relative;
 
+    label {
+        position: absolute;
+        top: -2.25rem;
+    }
+
+}
+
+.form-group-inline {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    position: relative;
+
+    margin: 2.5rem 0;
+
+    @media screen and (min-width: 767px) {
+        flex-direction: row;
+
+    }
+
+    .form-group {
+        
+        margin-bottom: 2.25rem;
+
+        input, select {
+            min-width: 20rem;
+            height: 1.5rem;
+
+            border-radius: 0.25rem;
+
+            border: 1px solid var(--black);
+        }
+
+        input {
+            @media (min-width: 767px){
+                border-top-right-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+        }
+
+        select {
+            height: 1.75rem;
+
+              
+
+            @media (min-width: 767px){
+                border-radius: 0;   
+            }    
+        }
+
+        @media screen and (min-width: 767px) {
+            margin-bottom: 0;            
+        }
+    }
+
+    .btn {
+        @media screen and (min-width: 767px) {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;           
+        }   
+    }
+}
+
+.btn {
     border: none;
-}
+    background-color: var(--blue);
+    border-radius: 0.25rem;
 
+    color: var(--grey);
+    font-weight: 700;
+
+    &:hover, &:focus {
+        background-color: var(--blued);
+    }
+}
 </style>
