971d2d77eeced4eb8491181849b6fb135f754438
mlpeck <mlpeck@arkbluecross.com>
2021-05-07 11:39:43 -0500

    adding info to possibly add connect/disconnect message

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,8 +3,10 @@
         <div class="inner">
             <div class="chat-bubble" v-for="(msg, index) in messages" :key="index" :class="msg.message.class">
                 <span class="name">{{msg.message.name}}</span>
-                <p>{{msg.message.message}}</p></div>
+                <p>{{msg.message.message}}</p>
+            </div>
         </div>
+        
         <form class="form-wrapper">
             <div class="form-group">
                 <label for="message">Message:</label>
@@ -28,16 +30,16 @@ export default {
             message: '',
             messages: [],
             bubbleClass:"",
-            socket : io('localhost:3000')
+            socket : io('localhost:3000'), 
+            connectMessage: ''
         }
     },
     methods: {
         sendMessage(e) {
             const messageInput = document.getElementById('message').value;
             e.preventDefault();
-
             if(messageInput){
-               this.socket.emit('SEND_MESSAGE', {
+               this.socket.emit('send', {
                 user: this.type,
                 message: this.message,
                 class: this.bubbleClass,
@@ -51,26 +53,24 @@ export default {
     created(){
         this.username = this.$route.params.data.name;
         this.type = this.$route.params.data.type;
+        this.connectMessage = this.$route.params.data.name + ' has joined the chat.';
     if(this.type == "agent"){
                 this.bubbleClass = 'chat-bubble--agent';
             }
             else {
                 this.bubbleClass='chat-bubble--client';
             }
-        this.socket.on('MESSAGE', (data)=>{
-           
+        this.socket.on('message', (data)=>{
             this.messages.push({
                 message: data, 
                 user: this.type,
-                class: this.bubbleClass
+                class: this.bubbleClass,
+                connectMsg : this.connectMessage
             });
 
         });
-
-         this.socket.on('connect', ()=>{
-            console.log(this.type+' connected')
-        });
     }
+    
 }
 </script>
 
