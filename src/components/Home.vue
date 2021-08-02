5b10c46ffa574e2b3067c3e53fc2669d54c6aea3
mlpeck <mlpeck@arkbluecross.com>
2021-08-02 14:14:31 -0500

    creating a custom select

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,13 +13,13 @@
         <label for="name">Name</label>
         <input type="text" name="name" id="name" />
       </div>
-      <div class="form-group">
-        <label for="type">User type</label>
-        <select name="type" id="type">
-          <option value="Select user type">Select user type</option>
-          <option value="agent">Agent</option>
-          <option value="client">Client</option>
-        </select>
+      <div class="form-group custom-select">
+        <label for="user" id="user">User type</label>
+        <button class="select-toggle" type="button" aria-haspopup="true" @click='customSelect' id="type">Select user type</button>
+        <ul class="select" role="listbox" aria-expanded="false"  aria-labelledby="user">
+          <li role="option" value="agent" tabindex="0" aria-selected="false">Agent</li>
+          <li role="option" value="client" tabindex="0" aria-selected="false">Client</li>
+        </ul>
       </div>
       <div class="error-wrapper" v-if="errorMsgs[0]" >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>
@@ -27,7 +27,6 @@
           <li v-for="(error, index) in errorMsgs" :key="index">{{ error }}</li>
         </ul>
       </div>
-
     </div> 
     <input type="submit" value="Chat" @click="storeData" class="btn" />
   </div>
@@ -48,7 +47,7 @@ export default {
       e.preventDefault();
 
       const name = document.querySelector("#name").value;
-      const userType = document.querySelector("#type").value;
+      const userType = document.querySelector("#type").textContent;
       this.errorMsgs = [];
       if (name === null || name === undefined || name === "" || name === " ") {
         this.errorMsgs.push("Please enter a name");
@@ -75,6 +74,37 @@ export default {
         });
       }
     },
+    customSelect(){
+      const toggler = document.querySelector('.select-toggle');
+      const select = document.querySelector('.select');
+  
+        const selectOptions = select.children;
+
+        [...selectOptions].forEach(item => {
+          item.setAttribute('aria-selected', false);
+            
+            item.addEventListener ('click', function(){
+              const value = item.textContent;
+              toggler.textContent = value;
+              this.value = value;
+              if(item.getAttribute('aria-selected') !== 'true'){
+                item.setAttribute('aria-selected', true);
+              }
+
+              select.setAttribute('aria-expanded', false);
+              toggler.classList.remove('active');
+            });
+        });
+      
+       if(select.getAttribute('aria-expanded') !== 'true'){
+          select.setAttribute('aria-expanded', 'true'); 
+          toggler.classList.add('active');
+        }
+        else {
+          select.setAttribute('aria-expanded', false);
+          toggler.classList.remove('active');
+        }
+    }
   },
 };
 </script>
@@ -101,8 +131,8 @@ export default {
 }
 
 .error-wrapper {
-  width: calc(100% - 3.5rem);
-  height: 4.5rem;
+  width: calc(100% - 3.100%);
+  height: 4.100%;
 
   display: flex;
   align-items: center;
@@ -163,6 +193,8 @@ export default {
         flex-grow: 1;
 
         border-radius: 0;
+        border: 2px solid var(--black);
+
     }
 
     select {
@@ -192,4 +224,132 @@ export default {
         background-color: var(--blued);
     }
 }
+
+.custom-select {
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+
+  cursor: pointer;
+
+  *:before, &:after {
+    box-sizing: border-box;
+  }
+
+  .select-toggle {
+    appearance: none;
+
+    position: relative;
+    
+    padding: 0.75rem 2rem 0.75rem 0.75rem;
+
+    background: transparent;
+    color: var(--black);
+    border: 2px solid var(--black);
+
+    font-family: var(--body-font);
+    font-size: 1rem;
+    text-align: left;
+
+    &:after {
+      content: '';
+      
+      height: 0;
+      width: 0;
+
+      position: absolute;
+      right: 1rem;
+      top: 1rem;
+
+      border-left: 0.5rem solid transparent;
+      border-right: 0.5rem solid transparent;
+      border-top: 0.75rem solid var(--blue);
+
+    }
+
+    &.active {
+      &:after {
+        border-top: 0;
+        border-bottom: 0.75rem solid var(--blue);
+      }
+    }
+  }
+
+  .select {
+  
+    padding: 0;
+    margin:0;
+
+    list-style-type: none;
+
+    background: #fff;
+    box-shadow: 0 1px 4px rgba(150, 150, 150, 0.65);
+
+    text-align: left;
+
+    z-index: 10;
+
+    visibility: hidden;
+
+    li {
+      display: flex;
+      flex-direction: column;
+
+      padding: 1rem 0.75rem;
+
+      border-bottom: 1px solid var(--grey);
+
+      &:last-of-type {
+        border-bottom: 0;
+      }
+
+      &:hover, &:focus {
+        background-color: lighten(#e3e3e2, 8%);
+    }
+}
+  }
+
+  .select[aria-expanded="true"] {
+    visibility: visible;
+  }
+}
+
+// .custom-select{
+//   display: flex;
+
+//   position: relative;
+
+//   &:after {
+//     content: '';
+
+//     height: 0;
+//     width: 0;
+
+//     position: absolute;
+//     right: 1rem;
+//     top: 40%;
+//     bottom: 0;
+
+
+//     border-left: 0.45rem solid transparent;
+//     border-right: 0.45rem solid transparent;
+//     border-top: 0.90rem solid var(--blue);
+
+//     pointer-events: none;
+//   }
+
+//   select {
+//     padding: 0.55rem;
+
+//     appearance: none;  
+//     ::-ms-expand {
+//       display: none;
+//     }
+
+//     option {
+//       background-color: red;
+//     }
+//   } 
+// }
 </style>
