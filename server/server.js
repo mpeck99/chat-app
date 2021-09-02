3fb0090e03dbd2247265cb16ccbdc2b2f7306335
mlpeck <mlpeck@arkbluecross.com>
2021-09-02 10:22:07 -0500

    testing

--- server/server.js
+++ server/server.js
@@ -4,18 +4,13 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = 'index.html';
+const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server, {
-    cors :  {
-        origin: '*',
-        methods: ["GET", "POST"]
-    }
-});
+const io = socketIO(server);
 
 io.on('connection', function(socket) {
     socket.join('chat');
