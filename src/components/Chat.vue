5ed80a97dfa0a72c8d3be45653023996f9bc893d
mlpeck <mlpeck@arkbluecross.com>
2021-10-22 17:04:42 -0500

    Test

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -33,7 +33,7 @@
 
 <script>
 import io from "socket.io-client";
-var socket = io('chat.morganpeck.com', {transports: ['websocket'], upgrade: false});
+var socket = io('https://radiant-atoll-76864.herokuapp.com', {transports: ['websocket'], upgrade: false});
 
 export default {
   props: ["name", "userType"],
