81534cddf471fbe4bfc4f97799a592ce75ea155e
mlpeck <mlpeck@arkbluecross.com>
2021-10-27 09:35:10 -0500

    tst

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  publicPath: '/',
-    chainWebpack: config => {
-      config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = 'Vue Chat App'
-        return args
-      })
-    }
-  }
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Vue Chat App'
+      return args
+    })
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/dist/'
+  : '/'
+}
