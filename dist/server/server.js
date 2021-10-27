2a1a0d2429f8c83a0aaa46d019810c6ff4436734
mlpeck <mlpeck@arkbluecross.com>
2021-10-27 10:01:38 -0500

    trying something else

--- dist/server/server.js
+++ dist/server/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use(express.static((req, res) => res.sendFile(INDEX, { root: __dirname })))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server, {
