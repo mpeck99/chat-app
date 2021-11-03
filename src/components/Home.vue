f871a2622beca95464fe53be001b3e4059471583
mlpeck <mlpeck@arkbluecross.com>
2021-11-03 17:34:32 -0500

    working on styling the radio buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,34 +1,63 @@
 <template>
   <div class="wrapper">
-    <h1>Chat</h1>
-    <p>
-      Welcome to my chat app. Please enter your name and select a user type and
-      click chat to continue.
-    </p>
-    <div class="form-wrapper">
+    <div class="inner">
+      <h1>Chat</h1>
+      <p>
+        Welcome to my chat app. Please enter your name and select a user type and
+        click chat to continue.
+      </p>
       <div class="form-group">
         <label for="name">Name</label>
         <input type="text" name="name" id="name" />
       </div>
-      <div class="form-group custom-select">
-        <label for="user" id="user">User type</label>
-        <button class="select-toggle" type="button" aria-haspopup="true" @click="customSelect" id="type"
-        >Select user type</button>
-        <ul class="select" role="listbox" aria-expanded="false" aria-labelledby="user">
-          <li role="option" value="agent" tabindex="0" aria-selected="false">Agent</li>
-          <li role="option" value="client" tabindex="0" aria-selected="false">Client</li>
-        </ul>
+      <div class="fieldset fieldset--radio">
+        <legend>Who are you?</legend>
+        <div class="form-group form-group--radio checked">
+          <label for="client">
+            <input
+              type="radio"
+              id="client"
+              name="type"
+              value="client"
+              @click="addChecked"
+              v-model="checked"
+            />
+            Client
+          </label>
+        </div>
+        <div class="form-group form-group--radio" id="type">
+          <label for="agent"
+            ><input
+              type="radio"
+              id="agent"
+              name="type"
+              value="agent"
+              @click="addChecked"
+            />
+            Agent
+          </label>
+        </div>
       </div>
       <div class="error-wrapper" v-if="errorMsgs[0]">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        ><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <line x1="12" y1="8" x2="12" y2="12"></line>
+          <line x1="12" y1="16" x2="12.01" y2="16"></line>
         </svg>
         <ul class="errors">
           <li v-for="(error, index) in errorMsgs" :key="index">{{ error }}</li>
         </ul>
       </div>
+      <input type="submit" value="Chat" @click="storeData" class="btn" />
     </div>
-    <input type="submit" value="Chat" @click="storeData" class="btn" />
   </div>
 </template>
 
@@ -39,6 +68,7 @@ export default {
       name: "",
       userType: "",
       errorMsgs: [],
+      checked: 'client',
     };
   },
 
@@ -47,76 +77,49 @@ export default {
       e.preventDefault();
 
       const name = document.querySelector("#name").value;
-      const userType = document.querySelector("#type").textContent;
+
       this.errorMsgs = [];
       if (name === null || name === undefined || name === "" || name === " ") {
         this.errorMsgs.push("Please enter a name");
       } else {
         this.name = name;
       }
-
-      if (userType != "Select user type") {
-        this.userType = userType;
-      } else {
-        this.errorMsgs.push("Please select a user type.");
-      }
+      console.log(this.userType);
 
       if (this.errorMsgs.length == 0) {
         localStorage["data"] = JSON.stringify({
           name: this.name,
           type: this.userType,
         });
-        
-        if (this.userType === 'Agent') {
-          this.windowRef = window.open(
-            "/agent",
-            "",
-            "width=900,height=600"
-          );
+
+        if (this.userType === "agent") {
+          this.windowRef = window.open("/agent", "", "width=900,height=600");
           this.$router.resolve({
             path: "/agent",
-            
           });
         } else {
-          this.windowRef = window.open(
-            "/chat",
-            "",
-            "width=400,height=600"
-          );
+          this.windowRef = window.open("/chat", "", "width=400,height=600");
           this.$router.resolve({
             path: "/chat",
           });
         }
       }
     },
-    customSelect() {
-      const toggler = document.querySelector(".select-toggle");
-      const select = document.querySelector(".select");
+    addChecked(e) {
+      const userType = e.target;
+      const parent = userType.parentElement.parentElement;
+      const allRadios = document.querySelectorAll('input[type="radio"]');
 
-      const selectOptions = select.children;
-
-      [...selectOptions].forEach((item) => {
-        item.setAttribute("aria-selected", false);
-
-        item.addEventListener("click", function () {
-          const value = item.textContent;
-          toggler.textContent = value;
-          this.value = value;
-          if (item.getAttribute("aria-selected") !== "true") {
-            item.setAttribute("aria-selected", true);
-          }
-
-          select.setAttribute("aria-expanded", false);
-          toggler.classList.remove("active");
-        });
-      });
-
-      if (select.getAttribute("aria-expanded") !== "true") {
-        select.setAttribute("aria-expanded", "true");
-        toggler.classList.add("active");
-      } else {
-        select.setAttribute("aria-expanded", false);
-        toggler.classList.remove("active");
+      if (userType.checked) {
+        if (!parent.classList.contains("checked")) {
+          parent.classList.add("checked");
+          this.userType = userType.value;
+          allRadios.forEach((element) => {
+            if (element != userType) {
+              element.parentElement.parentElement.classList.remove("checked");
+            }
+          });
+        }
       }
     },
   },
@@ -156,15 +159,8 @@ export default {
   }
 }
 
-.form-wrapper {
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-
-  position: relative;
-
-  padding: 1.5rem;
+.inner {
+  width: 80%;
 }
 
 .form-group {
@@ -174,22 +170,20 @@ export default {
 
   position: relative;
 
-  margin: 1.75rem 0;
+  margin: 3.25rem 0 0 0;
 
   label {
     position: absolute;
     top: -2.25rem;
   }
 
-  input,
-  select,
-  .form-control {
+  input[type="text"] {
     height: 2.5rem;
 
     flex-grow: 1;
 
     border-radius: 0;
-    border: 2px solid var(--black);
+    border: 2px solid var(--grey);
 
     box-shadow: 0 1px 4px rgba(150, 150, 150, 0.65);
 
@@ -201,7 +195,8 @@ export default {
   }
 }
 
-.btn, input[type="submit"] {
+.btn,
+input[type="submit"] {
   width: 100%;
   min-height: 2.5rem;
 
@@ -219,7 +214,7 @@ export default {
   font-weight: 700;
   font-size: 1.25rem;
 
-  -webkit-appearance:none;
+  -webkit-appearance: none;
 
   &:hover,
   &:focus {
@@ -227,108 +222,89 @@ export default {
   }
 }
 
-.custom-select {
+.fieldset--radio {
+  width: 100%;
+
   display: flex;
   flex-direction: column;
+  justify-content: center;
 
-  max-height: 2.75rem;
+  margin-top: 2rem;
 
-  position: relative;
+  text-align: left;
 
-  cursor: pointer;
+  legend {
+    grid-row: 1 / 2;
+    grid-column: 1 / 3;
 
-  box-shadow: 0 1px 4px rgba(150, 150, 150, 0.65);
-
-  *:before,
-  &:after {
-    box-sizing: border-box;
+    margin-bottom: 1rem;
   }
 
-  .select-toggle {
-    appearance: none;
+  .form-group--radio {
+    width: 11.5rem;
 
-    position: relative;
-
-    padding: 0.75rem 2rem 0.75rem 0.75rem;
-
-    background: transparent;
-    color: var(--black);
-    border: 2px solid var(--black);
-
-    font-family: var(--body-font);
-    font-size: 1rem;
-    text-align: left;
-
-    &:after {
-      content: "";
-
-      height: 0;
-      width: 0;
-
-      position: absolute;
-      right: 1rem;
-      top: 1rem;
-
-      border-left: 0.5rem solid transparent;
-      border-right: 0.5rem solid transparent;
-      border-top: 0.75rem solid var(--blue);
-    }
-
-    &.active {
-      &:after {
-        border-top: 0;
-        border-bottom: 0.75rem solid var(--blue);
-      }
-    }
-
-    &:hover,
-    &:focus {
-      cursor: pointer;
-
-      border-color: var(--blue);
-    }
-  }
-
-  .select {
-    padding: 0;
+    align-self: center;
     margin: 0;
 
-    list-style-type: none;
+    label {
+      width: 100%;
 
-    background: #fff;
-    box-shadow: 0 1px 4px rgba(150, 150, 150, 0.65);
-
-    text-align: left;
-
-    z-index: 10;
-
-    visibility: hidden;
-    opacity: 0;
-
-    transition: visibility 0.3s linear, opacity 0.3s linear;
-
-    li {
       display: flex;
-      flex-direction: column;
 
-      padding: 1rem 0.75rem;
+      position: static;
 
-      border-bottom: 1px solid var(--grey);
+      padding: 1rem;
 
-      &:last-of-type {
-        border-bottom: 0;
+      box-shadow: 0 1px 4px rgba(150, 150, 150, 0.65);
+      background-color: var(--white);
+      border: 2px solid var(--grey);
+      color: var(--greyD);
+    }
+
+    &.checked {
+      label {
+        background-color: var(--blue);
+        border: none;
+        color: var(--white);
       }
+    }
 
-      &:hover,
-      &:focus {
-        background-color: lighten(#e3e3e2, 8%);
+    input[type="radio"] {
+      width: 2rem;
+      height: 2rem;
+
+      margin: 0 1rem 0 0;
+
+      border: 0.2rem solid var(--grey);
+      border-radius: 50%;
+
+      transition: 0.2s all linear;
+
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+
+      &:checked {
+        border: 0.5rem solid var(--white);
       }
+    }
+
+    &:first-of-type {
+      grid-row: 2 / 3;
+      grid-column: 1 / 2;
+    }
+    &:last-of-type {
+      grid-row: 2 / 3;
+      grid-column: 2 / 3;
     }
   }
 
-  .select[aria-expanded="true"] {
-    visibility: visible;
-    opacity: 1;
+  @media (min-width: 767px) {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-template-rows: auto auto;
+
+    justify-content: space-between;
   }
 }
 </style>
