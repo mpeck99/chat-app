ba8f9b5b17e8b45d35a21eb4b6baa322301bc322
mlpeck <mlpeck@arkbluecross.com>
2021-11-11 08:16:16 -0600

    adding comments and the chatbubble css

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ export default {
 };
 </script>
 <style lang="scss">
+// Structure/base styles
 :root {
   --body-font: "Roboto", sans-serif;
   --header-font: "Oswald", sans-serif;
@@ -110,6 +111,30 @@ input {
   }
 }
 
+.sr-only {
+  border: 0;
+  padding: 0;
+  margin: 0;
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(
+    1px 1px 1px 1px
+  ); /* IE6, IE7 - a 0 height clip, off to the bottom right of the visible 1px box */
+  clip: rect(
+    1px,
+    1px,
+    1px,
+    1px
+  ); /*maybe deprecated but we need to support legacy browsers */
+  clip-path: inset(
+    50%
+  ); /*modern browsers, clip-path works inwards from each corner*/
+  white-space: nowrap;
+}
+
+// Form styles
 .btn,
 input[type="submit"] {
   width: 100%;
@@ -134,34 +159,12 @@ input[type="submit"] {
     background-color: var(--blued);
   }
 }
-.sr-only {
-  border: 0;
-  padding: 0;
-  margin: 0;
-  position: absolute !important;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(
-    1px 1px 1px 1px
-  ); /* IE6, IE7 - a 0 height clip, off to the bottom right of the visible 1px box */
-  clip: rect(
-    1px,
-    1px,
-    1px,
-    1px
-  ); /*maybe deprecated but we need to support legacy browsers */
-  clip-path: inset(
-    50%
-  ); /*modern browsers, clip-path works inwards from each corner*/
-  white-space: nowrap;
-}
 
 textarea, input {
   outline: none;
 
   &:hover, &:focus {
-    border-color: var(--blue);
+    border-color: var(--blue) !important;
   }
 }
 
@@ -171,6 +174,126 @@ textarea, input {
    label {
     position: absolute;
     top: -2.25rem;
+  }
+}
+
+// Chat bubble 
+.chat-bubble {
+  max-width: 80%;
+
+  min-width: 4rem;
+
+  padding: 0.5rem 1rem;
+  margin: 2rem 1rem;
+
+  position: relative;
+
+  background: var(--grey);
+
+  p {
+    max-width: 100%;
+
+    margin: 0;
+
+    word-break: break-all;
+    text-align: right;
+  }
+
+  .name {
+    position: absolute;
+    bottom: -1.5rem;
+    right: 0;
+
+    font-size: 0.9rem;
+    text-transform: capitalize;
+    font-family: var(--header-font);
+  }
+
+  &:after {
+    content: "";
+
+    width: 0;
+    height: 0;
+
+    position: absolute;
+    bottom: 0;
+    right: -1rem;
+
+    border-bottom: 1.25rem solid var(--grey);
+    border-right: 1.25rem solid transparent;
+  }
+
+  &.disclaimer {
+    max-width: 100%;
+
+    padding: 0;
+    margin-bottom: 0;
+
+    list-style-type: none;
+
+    background-color: transparent;
+    color: darken(#ebebeb, 50%);
+    font-style: italic;
+    font-size: 0.9rem;
+
+    &:after {
+      display: none;
+    }
+  }
+}
+
+// Typing
+.typing {
+  height: 1.5rem;
+  width: 3rem;
+
+  display: none;
+
+  position: absolute;
+
+  background: var(--grey);
+  // border-radius: 3.125rem;
+
+  &.active {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    span {
+      animation: typing 1.5s infinite ease-in-out;
+
+      &:nth-of-type(2){
+        animation-delay: 100ms;
+      }
+      &:nth-of-type(3){
+        animation-delay: 200ms;
+      }
+    }
+  }
+
+  span {
+    width: 0.5rem;
+    height: 0.5rem;
+
+    display: block;
+
+    margin: 0 0.1rem;
+
+    border-radius: 3.125rem;
+    background: var(--greyD);
+  }
+}
+
+// Keyframes
+@keyframes typing {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-0.2rem);
+  }
+  100% {
+    transform: translateY(0);
   }
 }
 </style>
